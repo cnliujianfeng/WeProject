ce2dd10e28e6e480a3a5be501c238bf4293ff84e
cnliujianfeng <2214414345@qq.com>
2018-06-04 17:48:33 +0800

    添加了Toolbar的滑动和ViewPager的滑动绑定！

--- app/src/main/java/peproject/whrj/com/weproject/MainActivity.java
+++ app/src/main/java/peproject/whrj/com/weproject/MainActivity.java
@@ -2,41 +2,63 @@ package peproject.whrj.com.weproject;
 
 
 import android.content.Intent;
+import android.graphics.drawable.Icon;
 import android.net.Uri;
 import android.provider.ContactsContract;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.GravityCompat;
 
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.TintTypedArray;
+
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
+import adapter.peproject.whrj.com.weproject.GoodsPagerAdapter;
 
 
 public class MainActivity extends AppCompatActivity {
 
 
     private DrawerLayout mDrawerLayout;
+    //内容视图
+    private ViewPager vp_content;//翻页视图对象
+    private TabLayout tab_title;//标签布局对象
+    private ArrayList<String> mTitleArray = new ArrayList<String>(); //标题图标
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        //Toolbar替换默认ActiviBar
         Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mTitleArray.add("商品");
+        mTitleArray.add("详情");
+        vp_content=findViewById(R.id.vp_content);//从布局文件中获取vp_content的翻页视图
+
+        initTabLayout();
+        initTabViewPager();
+
+
         //获取滑动布局
+
         mDrawerLayout=(DrawerLayout)findViewById(R.id.dr_layout);
         ActionBar actionBar=getSupportActionBar();
         if(actionBar!=null)
@@ -48,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
         //获取NavigationView控件
         NavigationView navView=(NavigationView)findViewById(R.id.nav_view);
        // navView.setCheckedItem(R.id.nav_call);
-        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+     navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId())
@@ -58,7 +80,6 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(intent);
                     break;
                     case R.id.nav_friend:
-
                         Uri uri = ContactsContract.Contacts.CONTENT_URI;
                         Intent intent1 = new Intent(Intent.ACTION_PICK,uri);
                         startActivityForResult(intent1,0);
@@ -90,8 +111,37 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+    //初始化标签
+    private void initTabLayout() {
+        //从布局文件中获取标签布局
+        tab_title=findViewById(R.id.tab_title);
+        tab_title.addTab(tab_title.newTab().setCustomView(R.layout.item_toolbar1));
+        TextView tv_toolbar1=findViewById(R.id.tv_toolbar1);
+        tv_toolbar1.setText(mTitleArray.get(0));
 
+        tab_title.addTab(tab_title.newTab().setCustomView(R.layout.item_toolbar2));
+        TextView tv_toolbar2 = findViewById(R.id.tv_toolbar2);
+        tv_toolbar2.setText(mTitleArray.get(1));
+        // 给tab_title添加标签选中监听器，该监听器默认绑定了翻页视图vp_content
+        tab_title.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(vp_content));
 
+    }
+
+    private void initTabViewPager() {
+        // 构建一个商品信息的翻页适配器
+        GoodsPagerAdapter adapter = new GoodsPagerAdapter(
+                getSupportFragmentManager(), mTitleArray);
+        // 给vp_content设置商品翻页适配器
+        vp_content.setAdapter(adapter);
+        // 给vp_content添加页面变更监听器
+        vp_content.addOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                // 选中tab_title指定位置的标签
+                tab_title.getTabAt(position).select();
+            }
+        });
+    }
 
     //加载菜单栏布局··
     @Override
---
+++ app/src/main/java/peproject/whrj/com/weproject/Titelimg.java
@@ -0,0 +1,15 @@
+package peproject.whrj.com.weproject;
+
+public class Titelimg {
+    private int imageId;
+    public Titelimg(int home) {
+        this.imageId=home;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+
+
+}
