c32c251bb738c21b84cd4eff0852b4f6a39c03fa
cnliujianfeng <2214414345@qq.com>
2018-06-06 00:46:49 +0800

    更改了一下布局结构

--- app/src/main/java/peproject/whrj/com/weproject/MainActivity.java
+++ app/src/main/java/peproject/whrj/com/weproject/MainActivity.java
@@ -24,11 +24,13 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -57,14 +59,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //Toolbar替换默认ActiviBar
         Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
+
+
+
         setSupportActionBar(toolbar);
         View decorview = getWindow().getDecorView();
-        if(Build.VERSION.SDK_INT>=21){//5.0以上的系统支持
-            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |View.SYSTEM_UI_FLAG_LAYOUT_STABLE;//表示让应用主题内容占据系统状态栏的空间
-            decorview.setSystemUiVisibility(option);
-            getWindow().setStatusBarColor(Color.parseColor("#00ffffff"));//设置状态栏颜色为透明
-
-        }
+        setStatus();
+//        if(Build.VERSION.SDK_INT>=21){//5.0以上的系统支持
+//            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |View.SYSTEM_UI_FLAG_LAYOUT_STABLE;//表示让应用主题内容占据系统状态栏的空间
+//            decorview.setSystemUiVisibility(option);
+//            getWindow().setStatusBarColor(Color.parseColor("#00ffffff"));//设置状态栏颜色为透明
+//
+//        }
 
 
         mTitleArray.add("商品");
@@ -135,6 +141,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    private void setStatus() {
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+    }
+
     //初始化标签
     private void initTabLayout() {
         //从布局文件中获取标签布局
@@ -170,7 +181,12 @@ public class MainActivity extends AppCompatActivity {
     //加载菜单栏布局··
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.toolbar,menu);
+
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.toolbar,menu);
+        MenuItem menuItem = menu.findItem(R.id.action_search);//在菜单中找到对应控件的item
+
+
         return true;
     }
     //菜单栏点击事件
@@ -184,6 +200,7 @@ public class MainActivity extends AppCompatActivity {
                 mDrawerLayout.openDrawer(GravityCompat.START);
                 break;
             }
+
             case R.id.back:
                 finish();//当点击返回后结束当前活动
                 break;
